2c323881ed565e066f74c651d5114279aa721769
rofound <tobelee@qq.com>
2022-12-01 11:46:20 +0800

    开发配置

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import BasicHeader from './components/BasicHeader.vue';
+const isDev = import.meta.env.DEV
 </script>
 
 <template>
+  <el-button v-if="isDev" v-for="route in $router.options.routes" @click="$router.replace(route.path)">{{route.path}}</el-button>
   <BasicHeader :title="$route.meta.title" />
   <div style="height: 10px" />
   <router-view />
