65ac2fa2f06ceb944e9d36bd25eb81f36eab6167
rofound <tobelee@qq.com>
2023-06-01 13:44:11 +0800

    生产打包到低代码平台<织信>

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,13 @@ import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 // element-ui end
 
+
 // https://vitejs.dev/config/
+
+// const isDev = process.env.npm_lifecycle_event;
+const isBuild = process.env.npm_lifecycle_event === 'build';
+// const isPreview = process.env.npm_lifecycle_event;
+console.log(isBuild)
 export default defineConfig({
   plugins: [
     vue(),
@@ -29,4 +35,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    assetsDir: "./",
+  },
+  ...(() => {
+    if (isBuild) return {
+      base: "https://table.cmpo1914.com/web/0Z5j5jWdHNeTR3C/file"
+    }
+  })()
 });
